9ad81fe544a11ea6635c54f0919c3212511b86ee
LeoZhang <117000082@qq.com>
2020-08-24 15:59:37 +0800

    add: template

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  router: {
+    // 自定义路由规则
+    extendRoutes (routes, resolve) {
+      routes.splice(0)
+      routes.push(...[
+        {
+          path: '/',
+          component: resolve(__dirname, 'pages/layout/'),
+          children: [
+            {
+              path: '',
+              name: 'home',
+              component: resolve(__dirname, 'pages/home/'),
+            },
+            {
+              path: '/login',
+              name: 'login',
+              component: resolve(__dirname, 'pages/login/'),
+            },
+            {
+              path: '/register',
+              name: 'register',
+              component: resolve(__dirname, 'pages/login/'),
+            },
+            {
+              path: '/profile/:username',
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/'),
+            },
+            {
+              path: '/settings',
+              name: 'settings',
+              component: resolve(__dirname, 'pages/settings/'),
+            },
+            {
+              path: '/editor',
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/'),
+            },
+            {
+              path: '/article/:slug',
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/'),
+            }
+          ]
+        },
+      ])
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
+}
